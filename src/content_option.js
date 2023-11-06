f1dc53fe3ec54ee0627e5535bf716ec3ccb4554e
karthik242002 <karthiksiva153@gmail.com>
2023-11-06 13:15:48 +0530

    initial commit

---
+++ src/content_option.js
@@ -0,0 +1,154 @@
+const logotext = "Karthii";
+const meta = {
+    title: "Karthik s",
+    description: "I’m Karthik S, An open source contributor,web3 learner, Backend Developer",
+};
+
+const introdata = {
+    title: "I’m Karthik S",
+    animated: {
+        first: "20 • Web3 Hackathon × 1🏆🏅",
+        second: "Intern at Yugaas tech",
+        third: "Open to work",
+    },
+    description: "An Open Source Contributor, Passionate for innovation, A boy trying to figure-out stuff!!",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About myself",
+    aboutme: "👋 Hi, I’m Karthii, I’m interested to recreate the coding, I’m currently learning B.E(CSE) in Jaya Engineering College, I’m looking to collaborate with new things."
+};
+const worktimeline = [{
+    jobtitle: "Instagram Clone",
+    where: "Current",
+    date: "2023",
+},
+{
+    jobtitle: "Nocode tools",
+    where: "Completed",
+    date: "2023",
+},
+{
+    jobtitle: "Google Clone",
+    where: "Completed",
+    date: "2022",
+},
+
+];
+
+const skills = [{
+    name: "Python",
+    value: 80,
+},
+{
+    name: "Html/Css",
+    value: 85,
+},
+{
+    name: "Javascript",
+    value: 80,
+},
+{
+    name: "React",
+    value: 60,
+},
+{
+    name: "Node.js",
+    value: 75,
+},
+];
+
+const services = [{
+    title: "UI & UX Design",
+    description: "We will create satisfying designs With the given Templates",
+},
+{
+    title: "Mobile Apps",
+    description: "A mobile app is currently working in progress with reclaim protocol",
+},
+{
+    title: "Wordpress Design",
+    description: "Currently working with the design of creating of E-commerce website.",
+},
+];
+
+const dataportfolio = [{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "Google clone using next js and Tailwind CSS.",
+    link: "https://github.com/karthik242002/E-ggle.git",
+},
+{
+    img: "https://picsum.photos/400/800/?grayscale",
+    description: "Education refers to the discipline that is concerned with methods of teaching and learning in schools or school- like environments, as opposed to various nonformal and informal means of socialization.",
+    link: "https://github.com/karthik242002/Educational_website.git",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "A simple chatbot created in a short time period",
+    link: "https://github.com/karthik242002/jec-chabot.git",
+},
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "50+ projects updated daily",
+    link: "https://github.com/karthik242002/50-Html-CSS-JS-Project.git",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "A simple multiplication app with funny experience",
+    link: "https://cross-game.netlify.app/",
+},
+{
+    img: "https://picsum.photos/400/700/?grayscale",
+    description: "IMDB clone using next js and Tailwind CSS",
+    link: "https://github.com/karthik242002/IMDB-clone.git",
+},
+
+{
+    img: "https://picsum.photos/400/600/?grayscale",
+    description: "Gmail clone using next js and Tailwind CSS",
+    link: "https://github.com/karthik242002/Gmail-clone.git",
+},
+{
+    img: "https://picsum.photos/400/300/?grayscale",
+    description: "It is a social media for a free photo and video sharing app. People can upload photos or videos to our service and share them with their followers or with a select group of friends. They can also view, comment and like posts shared by their friends on Instagram.",
+    link: "https://github.com/karthik242002/Instagram-clone.git",
+},
+{
+    img: "https://picsum.photos/400/?grayscale",
+    description: "Nocode Tools is project we developed for an web3 hackathon in amirta college with reclaim protocol.",
+    link: "https://github.com/karthik242002/Nocode_Tools_Amirta.git",
+},
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "karthiksiva153@gmail.com",
+    YOUR_PHONE: "(+91)9790744816",
+    description: "For Any Queries Mail : Gryffindorsred@gmail.com",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/karthik242002",
+    facebook: "https://www.facebook.com/karthik.siva.3517563",
+    linkedin: "https://www.linkedin.com/in/karthik-s-9a6121237/",
+    twitter: "https://twitter.com/karthik08917385",
+    instagram: "https://www.instagram.com/_furious_hearted_24._/"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
